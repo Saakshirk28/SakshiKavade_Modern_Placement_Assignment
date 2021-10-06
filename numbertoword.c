27b949799953deccba41a2595b77d974148d6f49
Saakshirk28 <92010635+Saakshirk28@users.noreply.github.com>
2021-10-06 15:35:54 +0530

    Update numbertoword.c

--- numbertoword.c
+++ numbertoword.c
@@ -1,57 +1,74 @@
-#include <stdio.h>
-
+#include<string.h>
+#include<stdio.h>
+#define BUF_SIZE 20
+// strings at index 0 is not used, it is to make array
+// indexing simple
+char* one[] = { "", "one ", "two ", "three ", "four ",
+                "five ", "six ", "seven ", "eight ",
+                "nine ", "ten ", "eleven ", "twelve ",
+                "thirteen ", "fourteen ", "fifteen ",
+                "sixteen ", "seventeen ", "eighteen ",
+                "nineteen "
+              };
+// strings at index 0 and 1 are not used, they is to
+// make array indexing simple
+char* ten[] = { "", "", "twenty ", "thirty ", "forty ",
+                "fifty ", "sixty ", "seventy ", "eighty ",
+                "ninety "
+              };
+char * numToWords(int n, char* s, char *str, int len)
+{
+    memset(str,0,len);
+    // if n is more than 19, divide it
+    if (n > 19)
+    {
+        strcat(str,ten[n / 10]);
+        strcat(str,one[n % 10]);
+    }
+    else
+    {
+        strcat(str,one[n]);
+    }
+    // if n is non-zero
+    if (n)
+    {
+        strcat(str,s);
+    }
+    return str;
+}
+// Function to print a given number in words
+char* convertToWords(long n, char *out)
+{
+    char str[BUF_SIZE] = {0};
+    // handles digits at ten millions and hundred
+    // millions places (if any)
+    strcat(out, numToWords((n / 10000000), "crore ",str,BUF_SIZE));
+    // handles digits at hundred thousands and one
+    // millions places (if any)
+    strcat(out, numToWords(((n / 100000) % 100), "lakh ",str,BUF_SIZE));
+    // handles digits at thousands and tens thousands
+    // places (if any)
+    strcat(out, numToWords(((n / 1000) % 100), "thousand ",str,BUF_SIZE));
+    // handles digit at hundreds places (if any)
+    strcat(out, numToWords(((n / 100) % 10), "hundred ",str,BUF_SIZE));
+    //Increase code readability
+    if (n > 100 && n % 100)
+    {
+        strcat(out, "and ");
+    }
+    // handles digits at ones and tens places (if any)
+    strcat(out, numToWords((n % 100), "",str,BUF_SIZE));
+    return out;
+}
 int main()
 {
-    int n, num = 0;
-
+    //Get input number from user
+    long num;
+    char str[60] = {0};
     
-    printf("Enter no: ");
-    scanf("%d", &n);
-
-    
-    while(n != 0)
-    {
-        num = (num * 10) + (n % 10);
-        n /= 10;
-    }
-    while(num != 0)
-    {
-        switch(num % 10)
-        {
-            case 0: 
-                printf("Zero ");
-                break;
-            case 1: 
-                printf("One ");
-                break;
-            case 2: 
-                printf("Two ");
-                break;
-            case 3: 
-                printf("Three ");
-                break;
-            case 4: 
-                printf("Four ");
-                break;
-            case 5: 
-                printf("Five ");
-                break;
-            case 6: 
-                printf("Six ");
-                break;
-            case 7: 
-                printf("Seven ");
-                break;
-            case 8: 
-                printf("Eight ");
-                break;
-            case 9: 
-                printf("Nine ");
-                break;
-        }
-        
-        num = num / 10;
-    }
-
+    printf("Enter any number: ");
+    scanf("%ld", &num);
+    // convert given number in words
+    printf( "%s",convertToWords(num,str));
     return 0;
 }
